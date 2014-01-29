e0d9702f3c7ab1a241eec82b51e37e6b1a162d41
Aras Balali Moghaddam <arasbm@gmail.com>
2014-01-29 14:56:37 -0800

    brand new xcode project

---
+++ OPiOSCordovaSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OPiOSCordovaSample
+//
+//  Created by Aras Balali Moghaddam on 1/29/2014.
+//  Copyright (c) 2014 Hookflash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OPiOSCordovaSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OPiOSCordovaSample
+//
+//  Created by Aras Balali Moghaddam on 1/29/2014.
+//  Copyright (c) 2014 Hookflash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
