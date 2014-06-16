e04f15062169491824f9f3ae0165811aeef16b28
Aras Balali Moghaddam <arasbm@gmail.com>
2014-06-16 16:41:10 -0700

    fix issue in download script and default link to OpenpeerSDK

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,5 @@
 
 cd ./src/ios/OpenpeerSDK.framework/             && \
 curl -v https://s3.amazonaws.com/assets.hookflash.me/github.com~hookflashco~hcs-stack-cordova/OpenpeerSDK_iOS_16June2014_v1.0.9.zip > OPSDK.zip && \
-unzip -fov OPSDK.zip                                  && \
+unzip -o OPSDK.zip                                  && \
 rm OPSDK.zip
