03efbb39765f0288dabf54cc54d876e3adb42c6f
Aras Balali Moghaddam <arasbm@gmail.com>
2014-06-16 16:29:18 -0700

    update prepare (download) script

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cd ./src/ios/OpenpeerSDK.framework/             && \
-curl -v https://s3.amazonaws.com/assets.hookflash.me/github.com~hookflashco~hcs-stack-cordova/OpenpeerSDK_iOS_3June2014_v1.0.9.zip > OPSDK.zip && \
+curl -v https://s3.amazonaws.com/assets.hookflash.me/github.com~hookflashco~hcs-stack-cordova/OpenpeerSDK_iOS_16June2014_v1.0.9.zip > OPSDK.zip && \
 unzip -fov OPSDK.zip                                  && \
 rm OPSDK.zip
-
